a0092e4d3dd3eb02d42e5e83074e142bf0f43794
Yosvel Reyes <yosvel.reyes@globant.com>
2021-10-14 18:12:26 -0300

    POC: adding redirection to ng app

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-import { cwd } from 'process';
 import express from 'express';
 import path, { dirname } from 'path';
-import { StatusCodes } from 'http-status-codes';
 
 const PORT = 8081;
 const app = express();
 
 const documentRoot = dirname(process.argv[1]);
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use('/app1', express.static(path.join(documentRoot, 'ngApp1', 'dist', 'ngApp1')));
-app.use('/app2', express.static(path.join(documentRoot, 'ngApp2', 'dist', 'ngApp2')));
 app.use('/landing', express.static(path.join(documentRoot, 'landing')))
 app.get('/', (_, res) => {
      res.redirect('/landing');
+});
+
+/**
+ * This method should be protected somehow... it need to be protected by serverside solution maybe.... will see the POC from Qiang
+ */
+
+app.post('/token', (req, res) => {
+     const token = req.body.token;
+     console.log(token);
+
+     res
+          .cookie('access_token', token, { expires: new Date(Date.now() + 8 * 3600000)})
+          .redirect(301, '/app1');
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
