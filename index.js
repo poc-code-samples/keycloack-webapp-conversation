f022a519e8335ac0924781264c8fed817ab0ea77
Yosvel Reyes <yrfonfria@gmail.com>
2021-10-15 08:53:04 -0300

    POC: more server side adjustments

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import express from 'express';
 import path, { dirname } from 'path';
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 
 const PORT = 8081;
 const app = express();
 
 const documentRoot = dirname(process.argv[1]);
 
+
+// Keycloack setup. 
+
+// configure the web session store to manage server-side authentication
+// the call to newKeycloack assume the keycloak.json file with client information is in the
+// same directory the app is being executed.
+const memoryStore = session.MemoryStore();
+const keycloak = new Keycloak({ store: memoryStore });
+
+app.set('trust proxy', true);
+//install the keycloak middleware
+app.use(keycloak.middleware());
+
+// End keycloak setup
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/app1', express.static(path.join(documentRoot, 'ngApp1', 'dist', 'ngApp1')));
+// this resource is protected by keycloak.
+app.use('/app1', keycloak.protect(), express.static(path.join(documentRoot, 'ngApp1', 'dist', 'ngApp1')));
+
+
 app.use('/landing', express.static(path.join(documentRoot, 'landing')))
 app.get('/', (_, res) => {
      res.redirect('/landing');
