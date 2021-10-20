823e96ee30b9023303ff88ad924490f2752039f1
Yosvel Reyes <yosvel.reyes@globant.com>
2021-10-20 13:09:10 -0300

    POC: Adding the landing page scripts and rename container

--- startKeycloak.sh
+++ startKeycloak.sh
@@ -2,7 +2,7 @@
 
 # Run a keycloack server in the 8080 port
 
-docker run -d --rm --name keycloak-server -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:15.0.2
+docker run -d --rm --name keycloak -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:15.0.2
 
 
 # Use the keycloak api to create 2 users.
